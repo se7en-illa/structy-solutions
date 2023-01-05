76ab0b0031f6d5e035ceeb1e65adeb51c7f7282a
Sarah Alli <desdequeensnyc@gmail.com>
2023-01-05 13:17:15 -0500

    uncompress

--- 1-array-string.js
+++ 1-array-string.js
@@ -10,3 +10,23 @@ for example, '2c' or '3a'.
 The function should return an uncompressed version of the string where each 'char' of a group is repeated 'number' times consecutively. 
 You may assume that the input string is well-formed according to the previously mentioned pattern.
 */
+
+const uncompress = (s) => {
+  let result = [];
+  const numbers = "0123456789";
+  let i = 0;
+  let j = 0;
+  while (j < s.length) {
+    if (numbers.includes(s[j])) {
+      j += 1;
+    } else {
+      const num = Number(s.slice(i, j));
+      for (let count = 0; count < num; count += 1) {
+        result.push(s[j]);
+      }
+      j += 1;
+      i = j;
+    }
+  }
+  return result.join("");
+};
