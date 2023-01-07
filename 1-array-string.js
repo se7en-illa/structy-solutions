07810513f817282b7e4dcfd9d5609107e8cb173f
Sarah Alli <desdequeensnyc@gmail.com>
2023-01-06 19:34:37 -0500

    anagrams

--- 1-array-string.js
+++ 1-array-string.js
@@ -87,3 +87,45 @@ const compress2 = (s) => {
 //   n = length of string
 //   Time: O(n)
 //   Space: O(n)
+
+/*
+anagrams
+
+Write a function, anagrams, that takes in two strings as arguments. 
+The function should return a boolean indicating whether or not the strings are anagrams. 
+Anagrams are strings that contain the same characters, but in any order.
+
+test_00:
+
+anagrams('restful', 'fluster'); // -> true
+*/
+
+const anagrams = (s1, s2) => {
+  const count = {};
+  for (let char of s1) {
+    if (!(char in count)) {
+      count[char] = 0;
+    }
+    count[char] += 1;
+  }
+
+  for (let char of s2) {
+    if (count[char] === undefined) {
+      return false;
+    } else {
+      count[char] -= 1;
+    }
+  }
+
+  for (let char in count) {
+    if (count[char] !== 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+//   n = length of string 1
+//   m = length of string 2
+//   Time: O(n + m)
+//   Space: O(n)
